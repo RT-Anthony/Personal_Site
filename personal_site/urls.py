41e96ca2bbe5aa6c73eff468ec02e413dfa79cd2
Anthony <anthony.bell.us@ieee.org>
2022-06-05 15:49:07 -0500

    blank index page

--- personal_site/urls.py
+++ personal_site/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
+
+
+def default(request):
+    return render(request, "base.html", {})
 
 urlpatterns = [
+    path("", default),
     path('admin/', admin.site.urls),
     path("blog/", include('blog.urls')),
     path("projects/", include("projects.urls")),
